c1f98610e0f62dbc36d9d0ce04004e26ee2e4a8c
Ac3CJ <cjgacay02@gmail.com>
2024-11-06 12:31:08 +0000

    Tachometer reading at 100% Duty Cycle

--- main.cpp
+++ main.cpp
@@ -22,6 +22,8 @@
     DigitalIn rotaryA(PA_1);
     DigitalIn rotaryB(PA_4);
 
+    //AnalogIn fanTACO(PA_0);
+
     // PWM Signals
     PwmOut fanPWM(PB_0);
 
@@ -30,6 +32,7 @@
 
     // Interrupts
     InterruptIn button(BUTTON1);
+    InterruptIn fanTACO(PA_0);
 #else
     bool led;
     bool extensionBoardLed;
@@ -38,9 +41,16 @@
 // Define the Serial USB Output
 BufferedSerial mypc(USBTX, USBRX);
 
-// These are GLOBAL Variables
+// =============================== CONSTANTS ===============================
+const int TEMP_SENS_ADDR = 0x9A; // I2C Address for temperature sensor
+
+// Microsecond conversions
+const int MILLISECOND = 1000;
+const int HALF_SECOND = 500000;
+
+// =============================== GLOBAL VARS ===============================
 short int rotaryEncoderStage = 0;
-const int tempSensAddr = 0x9A; // I2C Address for temperature sensor
+int fanTACOCounter = 0;
 
 FILE* mypcFile1 = fdopen(&mypc, "r+"); // Set up the Serial USB Ports
 
@@ -50,6 +60,11 @@ void flip()
 { // Flips extension board on call
     extensionBoardLed = !extensionBoardLed;
 } 
+
+void incrementTACO()
+{
+    fanTACOCounter++;
+}
 
 // =============================== OTHER FUNCTIONS ===============================
 
@@ -87,11 +102,18 @@ void rotaryEncoderDirectionLED()
     if ((rotaryEncoderStage != 0) && ((rotaryA) && (rotaryB))) rotaryEncoderStage = 0;
 }
 
+void readFanSpeed(int timeDelta) 
+{ // RPM = (TACO Ticks/2) / (Time converted from microseconds to minutes)
 
+    float fanRPM = ((float)fanTACOCounter/2) / ((float)timeDelta/(60000000));
+    int tempfanRPM = (int)round(fanRPM);
+    printf("Time Elapsed: %d\tFan RPM: %d\n", timeDelta, tempfanRPM);
+
+    fanTACOCounter = 0;
+}
 
 int openLoopControl()
 { // Check OneNote for details
-    int counter = 0;
 
     // Clockwise
     if ((rotaryEncoderStage == 0) && ((!rotaryA) && (rotaryB))) rotaryEncoderStage++;
@@ -103,8 +125,7 @@ int openLoopControl()
         biDirLedA = 1;
         biDirLedB = 0;
 
-        counter = 1;
-        //fanPWM.write(incrementalCounter);
+        return 1;
     }
 
     // Counter-Clockwise
@@ -117,14 +138,13 @@ int openLoopControl()
         biDirLedA = 0;
         biDirLedB = 1;
 
-        counter = -1;
-        //fanPWM.write(incrementalCounter)
+        return -1;
     }
 
     // For cases where the encoder is spun too quickly
     if ((rotaryEncoderStage != 0) && ((rotaryA) && (rotaryB))) rotaryEncoderStage = 0;
 
-    return counter;
+    return 0;
 }
 
 // I2C Interface Functions (Temperature Sensor)
@@ -156,16 +176,16 @@ char getTemperatureReading()
     // Display the temperature start sentence
     // Write to the tempSensAddress by modifying the last bit of the tempSensAddress 
     // and using a write command and write a "read temperature" command
-    tempSensorI2C.write(tempSensAddr, 0x00, 1); 
+    tempSensorI2C.write(TEMP_SENS_ADDR, 0x00, 1); 
 
     //Read the Data from the device by changing the last bit of the tempSensAddress to a read command
-    tempSensorI2C.read(tempSensAddr, tempSensData, 1); 
+    tempSensorI2C.read(TEMP_SENS_ADDR, tempSensData, 1); 
     wait_us(10);
 
 
     //-----------------Print out section ----------------------
     //Display device Address and informations
-    //fprintf(mypcFile1,"Device with tempSensAddress 0x%x with\r\n", tempSensAddr); 
+    //fprintf(mypcFile1,"Device with tempSensAddress 0x%x with\r\n", TEMP_SENS_ADDR); 
 
     //Prints out the result of Method 1
 
@@ -179,31 +199,64 @@ char getTemperatureReading()
 
 int main() 
 {
+    // Local Variables
+    char temperatureData;
+    float speedChangeValue = 0;
+
+    float fanSpeedPWM = 1.0f;
+
+    int tempFanSpeed = 0;
+    float fanSpeedRPM = 0.5f;
+    short int mainLoopCounter = 0;
+
+    Timer mainLoopTimer;
+
+    // Set initial states
     led = 0;
     extensionBoardLed = 0;
 
     button.mode(PullUp);
     button.rise(&flip);
 
-    // Temp PWM Stuff
-    fanPWM.period(0.1f);
-    fanPWM.write(1.0f);
+    fanTACO.mode(PullUp);
+    fanTACO.rise(&incrementTACO);
 
-    // Local Variables
-    char temperatureData;
-    int incrementalCounter = 0;
+    fanPWM.period(0.01f);
+    fanPWM.write(fanSpeedPWM);
 
+    // Start the timer and the main loop
+    mainLoopTimer.start();
     while (true) {
         // blinky();
         //rotaryEncoderDirectionLED();
 
         //temperatureData = getTemperatureReading();
+        
+        speedChangeValue = openLoopControl();
+        speedChangeValue /= 100;
 
-        incrementalCounter += openLoopControl();
-        printf("Counter Value: %d\n", incrementalCounter);
+        if (fanSpeedPWM + speedChangeValue > 1) fanSpeedPWM = 1.0f;
+        else if (fanSpeedPWM + speedChangeValue < 0) fanSpeedPWM = 0.0f;
+        else fanSpeedPWM += speedChangeValue;
 
+        tempFanSpeed = fanSpeedPWM * 100;
+        //printf("Counter Value: %d\n", tempFanSpeed);
+
+        fanPWM.write(fanSpeedPWM);
+        //fanSpeedRPM = readFanSpeed();
+
+        mainLoopCounter++;
+
+        if (mainLoopTimer.elapsed_time().count() >= HALF_SECOND) {
+            mainLoopTimer.stop();
+            printf("fanTACO: %d \t Fan PWM: %d\t", fanTACOCounter, tempFanSpeed);
+            readFanSpeed(mainLoopTimer.elapsed_time().count());
+
+            mainLoopTimer.reset();
+            mainLoopTimer.start();
+        }
     }
 }
 
 // Write the fan speed stuff
-// 
+// Fan Period could be altered to allow for low speed control by decreasing the period and duty cycle
